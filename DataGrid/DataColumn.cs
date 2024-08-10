14e0cef49b3578f3cf84509fa3e3cc56810e2ee1
Paul <paul70079@gmail.com>
2024-08-10 11:38:09 +0200

    Fix date viewing

--- DataGrid/DataColumn.cs
+++ DataGrid/DataColumn.cs
@@ -5,6 +5,7 @@ namespace DataGrid;
 public class DataColumn<TGridItem, TField> : BaseColumn<TGridItem>
 {
     [Parameter] public Func<TGridItem, TField>? Field { get; set; }
+    [Parameter] public Func<TField?, string?> DisplayFunc { get; set; } = x => x?.ToString();
 
     public override IEnumerable<TGridItem> Sort(IEnumerable<TGridItem> items, bool ascending) => ascending
         ? items.OrderBy(x => Field is null ? default : Field.Invoke(x))
@@ -15,5 +16,5 @@ public class DataColumn<TGridItem, TField> : BaseColumn<TGridItem>
         return Field?.Invoke(item)?.ToString()?.Contains(FilterText, StringComparison.InvariantCultureIgnoreCase) ?? string.IsNullOrEmpty(FilterText);
     }
 
-    public override RenderFragment Render(TGridItem item) => x => x.AddMarkupContent(0, Field?.Invoke(item)?.ToString());
+    public override RenderFragment Render(TGridItem item) => x => x.AddMarkupContent(0, DisplayFunc(Field is null ? default : Field.Invoke(item)) ?? string.Empty);
 }
